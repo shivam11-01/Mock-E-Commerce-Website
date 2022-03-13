203e9aba704541f14838fa61f26a3bfc626478e4
shivam11-01 <shivamnarkhede11@gmail.com>
2022-03-13 17:08:49 +0530

    initial commit

---
+++ script.js
@@ -0,0 +1,11 @@
+let phome = document.getElementById('phome');
+let pshop = document.getElementById('pshop');
+phome.style.display = "block";
+
+function changepage() {
+    if (phome.display.display == "block") {
+        return;
+    }
+
+
+}
